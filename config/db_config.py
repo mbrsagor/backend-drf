81ac00e77ca6a969a793873f0b747d8f26cd338b
Sagor <mbrsagor@gmail.com>
2020-07-23 20:19:29 +0600

    All of thing add to git

---
+++ config/db_config.py
@@ -0,0 +1,1 @@
+DATABASES = {    'default': {        'ENGINE': 'django.db.backends.mysql',        'NAME': 'manyToMany',        'USER': 'root',        'PASSWORD': '12345678',        'HOST': 'localhost',  # Or an IP Address that your DB is hosted on        'PORT': '3306',    }}
