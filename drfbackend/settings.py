f8ebbf2493dc630d722b45b9011abff17761571a
mbrsagor <mbrsagor@gmail.com>
2021-05-20 08:37:45 +0600

    App structure update

--- drfbackend/settings.py
+++ drfbackend/settings.py
@@ -38,18 +38,25 @@ ALLOWED_HOSTS = env('ALLOWED_HOSTS')
 
 # Application definition
 
-INSTALLED_APPS = [
+DEFAULT_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core',
+]
 
-    # 3rd party app
+THIRD_PARTY_APPS = [
     'rest_framework',
 ]
+
+LOCAL_APPS = [
+    'core',
+]
+
+INSTALLED_APPS = DEFAULT_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
