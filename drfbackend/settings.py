8d72f3548534c40475ff9a0080a37151cfeca5b5
mbrsagor <mbrsagor@gmail.com>
2021-05-20 08:31:54 +0600

    Configuration done

--- drfbackend/settings.py
+++ drfbackend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import environ
 from pathlib import Path
 from config.db_config import DATABASES
 
@@ -21,12 +22,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#m=lxeg7ffa-oat_qxazz=633jn-^@*2zepepc!_s0p11(4bj$'
+env = environ.Env(
+    DEBUG=(bool, False),
+    ALLOWED_HOSTS=(list),
+)
+envfile_path = os.path.join(BASE_DIR, '.env')
+environ.Env.read_env(envfile_path)
+
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env('ALLOWED_HOSTS')
 
 # Application definition
 
@@ -58,8 +66,7 @@ ROOT_URLCONF = 'drfbackend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,3 +133,15 @@ DEFAULT_FROM_EMAIL = 'abc@domain.com'
 SERVER_EMAIL = 'abc@domain.com'
 EMAIL_PORT = 25
 EMAIL_USE_TLS = False
+
+
+# CORS CONFIG #
+# CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ALLOW_METHODS = (
+#     'GET',
+#     'POST',
+#     'PUT',
+#     'DELETE',
+#     'OPTIONS',
+#     'PATCH',
+# )
