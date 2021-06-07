9918ce95eb9ab26a855a70e19d62b9fba56a2ea1
mbrsagor <mbrsagor@gmail.com>
2021-06-07 23:15:22 +0600

    New: Custom response message

---
+++ utils/response_message.py
@@ -0,0 +1,17 @@
+from django.utils.translation import gettext as _
+
+
+SUCCESSFULLY_CREATED_MSG = _("Successfully created")
+
+# exception
+HTTP_500_INTERNAL_SERVER_ERROR_MSG = _("Internal server error")
+
+# error
+DATA_VALIDATION_FAILED_MSG = _("Data validation error")
+
+EMPLOYEE_ID_REQUIRED_MSG = _("Employee ID is required field")
+EMPLOYEE_NOT_FOUND_MSG = _("Employee ID Not Found")
+
+PHONE_NUMBER_ERROR_MSG = _("Phone number is not valid")
+
+FILE_VALIDATION_ERROR_MSG = _("Please input Base64 valid data.")
