e66999738b9ce2eee4f98f80b3a51354f600fb20
mbrsagor <mbrsagor@gmail.com>
2022-03-20 11:49:06 +0600

    Refactor: Refactor core apps

--- drfbackend/settings.py
+++ drfbackend/settings.py
@@ -147,8 +147,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'accounts.User'
-
 # Celery configuration
 CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
 CELERY_RESULT_BACKEND = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
--- drfbackend/urls.py
+++ drfbackend/urls.py
@@ -23,8 +23,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
-    path('auth/', include('djoser.urls')),
-    path('auth/jwt/', include('djoser.urls.jwt')),
+    path('api/auth/', include('djoser.urls')),
+    # path('api/jwt/', include('djoser.urls.jwt')),
 ]
 
 if settings.DEBUG:
