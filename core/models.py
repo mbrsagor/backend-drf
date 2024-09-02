23896141fd05794cc6ba265f9c3696a6cfa8e762
Md.Bozlur Rosid Sagor <39002174+mbrsagor@users.noreply.github.com>
2024-09-02 22:47:50 +0600

    New: Follow following API for sponsor

--- core/models.py
+++ core/models.py
@@ -37,3 +37,14 @@ class Schedule(DomainEntity):
 
     def __str__(self):
         return self.name
+
+
+class Follow(DomainEntity):
+    follower = models.ForeignKey(User, related_name='following', on_delete=models.CASCADE)
+    following = models.ForeignKey(User, related_name='followers', on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('follower', 'following')
+
+    def __str__(self):
+        return f"{self.follower} follows {self.following}"
